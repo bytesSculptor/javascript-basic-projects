defe336835fcb2494bc39569d3f7d0a2a044e9d3
Byte Developer <145024847+bytesSculptor@users.noreply.github.com>
2023-10-07 05:11:49 +0000

    completed color changer project

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,31 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = [
+    'rgba(255, 0, 0, 0.5)',
+    'rgba(0, 255, 0, 0.5)',
+    'rgba(0, 0, 255, 0.5)',
+    'rgba(255, 255, 0, 0.5)',
+    'rgba(255, 0, 255, 0.5)',
+    'rgba(0, 255, 255, 0.5)',
+    'rgba(128, 128, 128, 0.5)',
+    'rgba(255, 127, 0, 0.7)',
+    'rgba(0, 255, 127, 0.7)',
+    'rgba(127, 0, 255, 0.7)',
+    'rgba(0, 127, 255, 0.7)',
+    'rgba(255, 0, 127, 0.7)',
+    'rgba(127, 255, 0, 0.7)',
+    'rgba(255, 255, 255, 0.3)',
+    'rgba(0, 0, 0, 0.8)'
+]
+
+
+const btn = document.getElementById("btn");
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function () {
+    const randomNumber = getRandomNumber();
+    color.textContent = colors[randomNumber]
+    document.body.style.backgroundColor = colors[randomNumber]
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,35 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const color = document.querySelector('.color')
+const btn = document.getElementById("btn");
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length)
+}
+
+btn.addEventListener('click', function () {
+    // console.log(getRandomNumber());
+    let hexColor = '#'
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()]
+    }
+    // console.log(hexColor);
+    document.body.style.backgroundColor = hexColor;
+    color.textContent = hexColor
+})
+
+
+// setInterval(() => {
+//     colorChanger()
+// }, 1000);
+
+// let colorChanger = function () {
+//     // console.log(getRandomNumber());
+//     let hexColor = '#'
+//     for (let i = 0; i < 6; i++) {
+//         hexColor += hex[getRandomNumber()]
+//     }
+//     // console.log(hexColor);
+//     document.body.style.backgroundColor = hexColor;
+//     color.textContent = hexColor
+// }
